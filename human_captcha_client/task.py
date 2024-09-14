8d7a460c11151cbe94e87a85a9bfab8577e4afba
Anchal Gurung <anchalghale@gmail.com>
2024-09-13 20:35:53 -0400

    feat: add logs

--- human_captcha_client/task.py
+++ human_captcha_client/task.py
@@ -2,11 +2,13 @@ from urllib.parse import urljoin
 
 import httpx
 
+from human_captcha_client import logger
 from human_captcha_client.auth import TokenAuth
 from human_captcha_client.constants import API_URL
 
 
 def request_captcha_task(auth: TokenAuth):
+    logger.info("Requesting new captcha task...")
     url = urljoin(API_URL, "api/captcha-tasks/request/?type=token")
     res = httpx.get(url, auth=auth, verify=False, timeout=30)
     res.raise_for_status()
